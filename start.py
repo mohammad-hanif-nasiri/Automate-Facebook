2dad333f6464c849f95ef340006b26601b85a545
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2024-11-25 08:06:28 +0430

    start.py file modified.

--- start.py
+++ start.py
@@ -35,14 +35,6 @@ users: Dict[str, Dict[str, str]] = {
         "--comment-count": "100",
         "--like-count": "0",
     },
-    "milad.noori.7860": {
-        "--page-url": "https://www.facebook.com/CityComputerStore",
-        "--username": "milad.noori.7860",
-        "--groups": "Math",
-        "--share-count": "150",
-        "--comment-count": "100",
-        "--like-count": "0",
-    },
 }
 
 threads: List[threading.Thread] = []
