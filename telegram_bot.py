98160a4cfa984d968c6a53473252e956cee5a384
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2024-11-30 19:29:52 +0430

    telegram_bot.py file modified.

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,8 +1,8 @@
 import asyncio
 import random
-from typing import Self, Set, Union
+from typing import Self, Set, Tuple, Union
 
-from telegram import Bot, PhotoSize
+from telegram import Bot, PhotoSize, Update
 from telegram._utils.types import FileInput
 
 from logger import logger
@@ -58,13 +58,30 @@ class TelegramBot:
                     "<y>Skipping</y> this chat ID and continuing with the next."
                 )  # Info about fallback
 
+    async def get_updates(
+        self: Self, timeout: int = 5
+    ) -> Union[Tuple[Update, ...], None]:
+        try:
+            updates = await self.bot.get_updates()
+            logger.success("<g>Successfully</g> updates retrieved.")
+
+            return updates
+
+        except Exception as _:
+            logger.error("<r>Failed</r> to get updates!")
+
+            if timeout > 0:
+                return await self.get_updates(timeout - 1)
+
     @property
     async def chat_ids(self: Self) -> Set[Union[str, int]]:
         ids: Set[Union[str, int]] = set()
+        updates = await self.get_updates()
 
-        for update in await self.bot.get_updates():
-            if update.message and update.message.chat_id:
-                ids.add(update.message.chat_id)
+        if updates:
+            for update in updates:
+                if update.message and update.message.chat_id:
+                    ids.add(update.message.chat_id)
 
         return ids
 
