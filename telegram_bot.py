fd4cf3a62f3cc38b2557e532ab47386b705d3637
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2024-11-30 16:19:23 +0430

    telegram_bot.py file modified.

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,9 +1,9 @@
 import asyncio
 import random
-from typing import Any, Dict, List, Self, Set
+from typing import Self, Set, Union
 
-import requests
-from telegram import Bot
+from telegram import Bot, PhotoSize
+from telegram._utils.types import FileInput
 
 from logger import logger
 
@@ -12,53 +12,62 @@ class TelegramBot:
 
     def __init__(self, token: str):
         self.bot: Bot = Bot(token)
+        self.token: str = token
 
     async def send_message(self: Self, text: str) -> None:
         logger.info("Attempting to send message to all chat IDs.")  # Start of method
 
-        for chat_id in self.chat_ids:
+        for chat_id in await self.chat_ids:
             try:
                 logger.info(
                     f"Sending message to chat_id: {chat_id}"
                 )  # Info about specific chat_id
                 await self.bot.send_message(chat_id, text)  # Send the message
                 logger.success(
-                    f"Message sent successfully to chat_id: {chat_id}"
+                    f"<b>Message</b> sent <g>successfully</g> to chat_id: <c>{chat_id}</c>"
                 )  # Success
                 await asyncio.sleep(2.5 + random.random())  # Throttle the requests
             except Exception as err:
                 logger.error(
-                    f"Failed to send message to chat_id: {chat_id}. Error: {err}"
+                    f"<r><b>Failed</b></r> to send message to chat_id: <c>{chat_id}</c>. Error: <r><b>{err}</b></r>"
                 )  # Log error
                 logger.error(
-                    "Skipping this chat ID and continuing with the next."
+                    "<y>Skipping</y> this chat ID and continuing with the next."
+                )  # Info about fallback
+
+    async def send_photo(
+        self: Self, photo: Union[FileInput, PhotoSize], caption: Union[str, None] = None
+    ) -> None:
+        logger.info("Attempting to send photo to all chat IDs.")  # Start of method
+
+        for chat_id in await self.chat_ids:
+            try:
+                logger.info(f"MSG")  # Info about specific chat_id
+                await self.bot.send_photo(chat_id, photo, caption)
+                logger.success(
+                    f"<b>Photo</b> sent <g>successfully</g> to chat_id: <c>{chat_id}</c>"
+                )  # Success
+                await asyncio.sleep(2.5 + random.random())  # Throttle the requests
+            except Exception as err:
+                logger.error(
+                    f"<r><b>Failed</b></r> to send <b>photo</b> to chat_id: <c>{chat_id}</c>. Error: <r><b>{err}</b></r>"
+                )  # Log error
+                logger.error(
+                    "<y>Skipping</y> this chat ID and continuing with the next."
                 )  # Info about fallback
 
     @property
-    def chat_ids(self: Self) -> Set[str]:
-        ids: Set[str] = set()
+    async def chat_ids(self: Self) -> Set[Union[str, int]]:
+        ids: Set[Union[str, int]] = set()
 
-        response: requests.Response = requests.get(
-            f"https://api.telegram.org/bot7989591590:{self.bot.token}/getUpdates"
-        )
-
-        if response.status_code == 200:
-            data = response.json()
-
-            result: List[Dict[str, Any]] = data.get("result", [])
-            ids.update(
-                set(map(lambda message: message["message"]["chat"]["id"], result))
-            )
+        for update in await self.bot.get_updates():
+            if update.message and update.message.chat_id:
+                ids.add(update.message.chat_id)
 
         return ids
 
 
-def main():
-    telegram_bot: TelegramBot = TelegramBot(
-        "7989591590:AAHh6kj2ezKKkWW-eLJqVxPZalI6B6flgyM"
-    )
-
-    asyncio.run(telegram_bot.send_message("Hi"))
+def main(): ...
 
 
 if __name__ == "__main__":
