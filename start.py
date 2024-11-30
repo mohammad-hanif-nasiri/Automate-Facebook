3909c7e18f39adb01a6236b2b89f06b6f0645813
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2024-11-30 19:06:58 +0430

    start.py file modified.

--- start.py
+++ start.py
@@ -4,35 +4,42 @@ from typing import Any, Dict, List
 
 from facebook import Facebook
 
-users: Dict[str, Dict[str, Any]] = {
-    "aliabdullah.nasiri": {
+users: List[Dict[str, Any]] = [
+    {
         "page_url": "https://www.facebook.com/CityComputerStore",
+        "username": "aliabdullah.nasiri",
         "groups": "Math",
         "share_count": 200,
         "comment_count": 250,
         "like_count": 0,
     },
-    "hanif.nasiri.1967": {
+    {
+        "page_url": "https://www.facebook.com/profile.php?id=61554947310688",
+        "username": "aliabdullah.nasiri",
+        "groups": "Math",
+        "share_count": 200,
+        "comment_count": 250,
+        "like_count": 0,
+    },
+    {
         "page_url": "https://www.facebook.com/profile.php?id=100063642170837",
+        "username": "hanif.nasiri.1967",
         "groups": "Math",
         "share_count": 150,
         "comment_count": 200,
         "like_count": 0,
     },
-    "ali.nasiri.20050727": {
+    {
         "page_url": "https://www.facebook.com/PaytakhtMobile",
+        "username": "ali.nasiri.20050727",
         "groups": "Math",
         "share_count": 200,
         "comment_count": 150,
         "like_count": 0,
     },
-}
+]
 
 threads: List[threading.Thread] = []
-
-for user, params in users.items():
-    for file in os.listdir("pkl/"):
-        pass
 
 for thread in threads:
     thread.start()
