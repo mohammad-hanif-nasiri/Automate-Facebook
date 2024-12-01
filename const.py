13c6756a6cbf6993affa8fda8e261587f7705c91
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2024-12-01 11:56:22 +0430

    new contants added

--- const.py
+++ const.py
@@ -1,1 +1,4 @@
 TELEGRAM_BOT_API_TOKEN: str = "7989591590:AAHh6kj2ezKKkWW-eLJqVxPZalI6B6flgyM"
+
+FONARTO_REGULAR_URL: str = "https://www.1001fonts.com/download/font/fonarto.regular.ttf"
+FONARTO_XT_URL: str = "https://www.1001fonts.com/download/font/fonarto.xt.ttf"
