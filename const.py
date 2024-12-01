d8e7923e4d3abbe8e0c752280863e1cd988b94a4
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2024-12-01 18:39:37 +0430

    more important constant added.

--- const.py
+++ const.py
@@ -8,6 +8,14 @@ FONARTO_XT_URL: str = "https://www.1001fonts.com/download/font/fonarto.xt.ttf"
 FONARTO_REGULAR_PATH: str = "/tmp/fonarto.regular.ttf"
 FONARTO_XT_PATH: str = "/tmp/fonarto.xt.ttf"
 
+GMAIL_USERNAME: str = "nasiri.aliabdullah@gmail.com"
+GMAIL_PASSWORD: str = "uvos gywy jnjx rrok"
+EMAIL_DEST: str = "nasiri.waliabdullah@gmail.com"
+
+TOR_PASSWORD: str = "Ali.Nasiri#88771234"
+TOR_PORT: str = "9050"
+TOR_CONTROL_PORT: str = "9051"
+
 
 download_file(FONARTO_REGULAR_URL, FONARTO_REGULAR_PATH)
 download_file(FONARTO_XT_URL, FONARTO_XT_PATH)
