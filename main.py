77022b610ed3bb5b285c250a59aa842a007c032d
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2024-12-01 18:54:44 +0430

    main.py file modified.

---
+++ main.py
@@ -0,0 +1,12 @@
+from const import (FONARTO_REGULAR_PATH, FONARTO_REGULAR_URL, FONARTO_XT_PATH,
+                   FONARTO_XT_URL)
+from functions import download_file
+
+
+def main():
+    download_file(FONARTO_REGULAR_URL, FONARTO_REGULAR_PATH)
+    download_file(FONARTO_XT_URL, FONARTO_XT_PATH)
+
+
+if __name__ != "__main__":
+    main()
