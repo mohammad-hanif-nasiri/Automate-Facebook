cee09269536621b28d5c42d4bc025d50a6d8e5db
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2024-12-01 15:32:15 +0430

    start.py file modified.

--- start.py
+++ start.py
@@ -1,4 +1,5 @@
 import os
+import re
 import threading
 from typing import Any, Dict, List
 
@@ -10,63 +11,68 @@ users: List[Dict[str, Any]] = [
         "page_url": "https://www.facebook.com/CityComputerStore",
         "username": "aliabdullah.nasiri",
         "groups": "Math",
-        "share_count": 200,
-        "comment_count": 250,
+        "share_count": 1,
+        "comment_count": 1,
         "like_count": 0,
     },
     {
         "page_url": "https://www.facebook.com/profile.php?id=61554947310688",
         "username": "aliabdullah.nasiri",
         "groups": "Math",
-        "share_count": 200,
-        "comment_count": 250,
+        "share_count": 1,
+        "comment_count": 1,
         "like_count": 0,
     },
     {
         "page_url": "https://www.facebook.com/profile.php?id=100063642170837",
         "username": "hanif.nasiri.1967",
         "groups": "Math",
-        "share_count": 150,
-        "comment_count": 200,
+        "share_count": 1,
+        "comment_count": 1,
         "like_count": 0,
     },
     {
         "page_url": "https://www.facebook.com/PaytakhtMobile",
         "username": "ali.nasiri.20050727",
         "groups": "Math",
-        "share_count": 200,
-        "comment_count": 150,
+        "share_count": 1,
+        "comment_count": 1,
         "like_count": 0,
     },
 ]
 
+files: List[str] = os.listdir("pkl/")
 threads: List[threading.Thread] = []
 
-for pkl in os.listdir("pkl"):
-    threads.append(
-        threading.Thread(
-            target=start,
-            kwargs=dict(
-                cookie_file=f"pkl/{pkl}",
-                page_url="https://www.facebook.com/PaytakhtMobile",
-                username=None,
-                groups=["Math"],
-                like_count=0,
-                share_count=1,
-                comment_count=1,
-                **dict(
-                    headless=True,
-                    disable_gpu=True,
-                    disable_extensions=True,
-                    disable_infobars=True,
-                    start_maximized=True,
-                    no_sandbox=True,
-                    incognito=True,
-                    block_notifications=True,
+for index, user in enumerate(users):
+    for file in files:
+        if not re.match(f"^{user['username']}.*", file):
+            continue
+
+        threads.append(
+            threading.Thread(
+                target=start,
+                kwargs=dict(
+                    cookie_file=f"pkl/{file}",
+                    page_url=user["page_url"],
+                    username=user["username"],
+                    groups=user["groups"],
+                    like_count=user["like_count"],
+                    comment_count=user["comment_count"],
+                    share_count=user["share_count"],
+                    **dict(
+                        headless=True,
+                        disable_gpu=True,
+                        disable_extensions=True,
+                        disable_infobars=True,
+                        start_maximized=True,
+                        no_sandbox=True,
+                        incognito=True,
+                        block_notifications=True,
+                    ),
                 ),
-            ),
+            )
         )
-    )
 
 for thread in threads:
     thread.start()
