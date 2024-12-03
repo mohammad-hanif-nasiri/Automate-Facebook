e14c997c40b38e534084c9ad9f1081888b8516af
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2024-12-03 14:29:14 +0430

    telegram_bot.py file modified.

--- telegram_bot.py
+++ telegram_bot.py
@@ -16,15 +16,20 @@ class TelegramBot:
         self.token: str = token
         self.updates: Union[Tuple[Update, ...], None] = None
 
-    async def send_message(self: Self, text: str) -> None:
+    async def send_message(
+        self: Self, text: str, chat_id: Union[int, None] = None
+    ) -> None:
         logger.info("Attempting to send message to all chat IDs.")  # Start of method
 
-        for chat_id in await self.chat_ids:
+        for _chat_id in await self.chat_ids:
+            if chat_id is not None and _chat_id != chat_id:
+                continue
+
             try:
                 logger.info(
                     f"Sending message to chat ID: {chat_id}"
                 )  # Info about specific chat_id
-                await self.bot.send_message(chat_id, text)  # Send the message
+                await self.bot.send_message(_chat_id, text)  # Send the message
                 logger.success(
                     f"<b>Message</b> sent <g>successfully</g> to chat ID: <c>{chat_id}</c>"
                 )  # Success
@@ -38,16 +43,22 @@ class TelegramBot:
                 )  # Info about fallback
 
     async def send_photo(
-        self: Self, photo: Union[FileInput, PhotoSize], caption: Union[str, None] = None
+        self: Self,
+        photo: Union[FileInput, PhotoSize],
+        caption: Union[str, None] = None,
+        chat_id: Union[int, None] = None,
     ) -> None:
         logger.info("Attempting to send photo to all chat IDs.")  # Start of method
 
-        for chat_id in await self.chat_ids:
+        for _chat_id in await self.chat_ids:
+            if chat_id is not None and _chat_id != chat_id:
+                continue
+
             try:
                 logger.info(
                     f"<b>Sending</b> photo to chat ID: <c>{chat_id}</c>"
                 )  # Info about specific chat_id
-                await self.bot.send_photo(chat_id, photo, caption)
+                await self.bot.send_photo(_chat_id, photo, caption)
                 logger.success(
                     f"<b>Photo</b> sent <g>successfully</g> to chat ID: <c>{chat_id}</c>"
                 )  # Success
@@ -60,16 +71,21 @@ class TelegramBot:
                     "<y>Skipping</y> this chat ID and continuing with the next."
                 )  # Info about fallback
 
-    async def send_photos(self: Self, *photos: InputMediaPhoto) -> None:
+    async def send_photos(
+        self: Self, *photos: InputMediaPhoto, chat_id: Union[int, None] = None
+    ) -> None:
         logger.info("Attempting to send all photos to all chat IDs.")
 
-        for chat_id in await self.chat_ids:
+        for _chat_id in await self.chat_ids:
+            if chat_id is not None and _chat_id != chat_id:
+                continue
+
             try:
                 logger.info(
                     f"<b>Sending</b> photos to chat ID: <c>{chat_id}</c>"
                 )  # info about specific chat_id
 
-                await self.bot.send_media_group(chat_id, photos)
+                await self.bot.send_media_group(_chat_id, photos)
 
                 logger.success(
                     f"<b>Photos</b> sent <g>successfully</g> to chat ID: <c>{chat_id}</c>"
