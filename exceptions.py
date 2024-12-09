1a40a45e6863c45bfb5edc59211d48611aa27f33
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2024-12-09 07:18:24 +0430

    exceptions.py file modified.

--- exceptions.py
+++ exceptions.py
@@ -8,3 +8,13 @@ class ShareLimitException(FBException):
 
 class CommentLimitException(FBException):
     pass
+
+
+class UserNotLoggedInException(Exception):
+    """
+    Exception raised when a user tries to perform an action without being logged in.
+    """
+
+    def __init__(self, message="User is not logged in. Please log in to continue."):
+        self.message = message
+        super().__init__(self.message)
