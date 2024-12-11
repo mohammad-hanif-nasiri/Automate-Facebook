69989a9c9cddfcb66c70c04d1da463a4a8fd145c
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2024-12-11 15:32:18 +0430

    start.py file modified.

--- start.py
+++ start.py
@@ -49,18 +49,6 @@ users: List[Dict[str, Any]] = [
         "send_invites": False,
         "telegram_id": 5906633627,
     },
-    {
-        "page_url": "https://www.facebook.com/CityComputerStore",
-        "username": "milad.noori.7860",
-        "credentials": None,
-        "groups": ["Math"],
-        "share_count": 100,
-        "comment_count": 50,
-        "like_count": 0,
-        "friend_request_count": 0,
-        "send_invites": False,
-        "telegram_id": 5906633627,
-    },
 ]
 
 files: List[str] = os.listdir("pkl/")
