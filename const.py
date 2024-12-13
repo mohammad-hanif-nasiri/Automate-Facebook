e9ddc874cfb960765fddf8bf4840cb7b7543932b
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2024-12-13 14:59:45 +0430

    APP_TITLE constant added.

--- const.py
+++ const.py
@@ -1,3 +1,5 @@
+APP_TITLE: str = "Automate Facebook"
+
 TELEGRAM_BOT_API_TOKEN: str = "7989591590:AAHh6kj2ezKKkWW-eLJqVxPZalI6B6flgyM"
 
 FONARTO_REGULAR_URL: str = "https://www.1001fonts.com/download/font/fonarto.regular.ttf"
