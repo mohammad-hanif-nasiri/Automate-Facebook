d64da8dabfc2c28d686c997cffbf9c5f0f735471
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2024-12-19 09:54:34 +0430

    logger module modified.

--- logger.py
+++ logger.py
@@ -1,6 +1,7 @@
 import sys
 
 from loguru import logger
+from chrome import Chrome
 
 format = (
     "<b>[<cyan>{time:YYYY-MM-DD HH:MM:SS A}</cyan>]</b> "
@@ -13,3 +14,23 @@ logger.add(".log", format=format, mode="w")
 logger.add(sys.stdout, format=format)
 
 logger = logger.opt(colors=True)
+
+
+class MyLogger:
+    def __init__(self, logger) -> None:
+        self._logger = logger
+
+    def error(self, message, **kwargs):
+        Chrome.report(message)
+        self._logger.error(message, **kwargs)
+
+    def warning(self, message, *args, **kwargs) -> None:
+        Chrome.report(message)
+        self._logger.warning(message, *args, **kwargs)
+
+    # Optional: Pass through all other methods
+    def __getattr__(self, item):
+        return getattr(self._logger, item)
+
+
+logger = MyLogger(logger)
