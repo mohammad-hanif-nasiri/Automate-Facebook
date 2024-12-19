6b366c09d0dbb209f99a533360c9500ee2d3d9b0
aliabdullahnasiri <nasiri.aliabdullah@gmail.com>
2024-12-19 09:54:23 +0430

    const.py file modified.

--- const.py
+++ const.py
@@ -1,6 +1,7 @@
 APP_TITLE: str = "Automate Facebook"
 
 TELEGRAM_BOT_API_TOKEN: str = "7989591590:AAHh6kj2ezKKkWW-eLJqVxPZalI6B6flgyM"
+TELEGRAM_BOT_API_TOKEN_DEBUG: str = "7415055730:AAEKbcTimGDGeq6ccjHSzgiDf1qL34NS60Y"
 
 FONARTO_REGULAR_URL: str = "https://www.1001fonts.com/download/font/fonarto.regular.ttf"
 FONARTO_XT_URL: str = "https://www.1001fonts.com/download/font/fonarto.xt.ttf"
